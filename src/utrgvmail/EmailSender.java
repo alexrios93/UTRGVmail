19df41c951af46fdd2e66f51d47c82617ada8e48
Alex <alejandro.c.rios01@utrgv.edu>
2016-12-10 15:11:46 -0600

    added an error catcher

--- src/utrgvmail/EmailSender.java
+++ src/utrgvmail/EmailSender.java
@@ -1,8 +1,7 @@
 package utrgvmail;
 
 import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import javafx.scene.control.Label;
 import javax.activation.DataHandler;
 import javax.activation.DataSource;
 import javax.activation.FileDataSource;
@@ -13,6 +12,8 @@ import javax.mail.Multipart;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.PasswordAuthentication;
+import javax.mail.internet.AddressException;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
@@ -24,18 +25,23 @@ public class EmailSender {
     // Constructors
     EmailSender() {        
     }
-    EmailSender(String fromEmail, String username, String password, String toEmail, String subject, String textMessage, String attachment) {
+    EmailSender(String fromEmail, String username, String password, String toEmail, String subject, String textMessage, String attachment, Label output) {
     }
     
-    public static void EmailSender(String fromEmail, String username, String password, String toEmail, String subject, String textMessage, String attachment) {
+    public static void EmailSender(String fromEmail, String username, String password, String toEmail, String subject, String textMessage, String attachment, Label output) {
         try {
             Properties props = new Properties();
             props.put("mail.smtp.host", "smtp.office365.com");
             props.put("mail.smtp.port","587");
-            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.auth", "true");  
             props.put("mail.smtp.starttls.enable", "true");
 
-            Session mailSession = Session.getInstance(props, null);
+            Session mailSession = Session.getInstance(props, new javax.mail.Authenticator() {                      
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(username, password);
+                }
+            });
+            
             mailSession.setDebug(true);
 
             Message emailMessage = new MimeMessage(mailSession);
@@ -46,8 +52,7 @@ public class EmailSender {
 
             // Create body part for the text message and attatchment
             BodyPart messageBodyPart = new MimeBodyPart();
-            BodyPart attatchmentBodyPart = new MimeBodyPart();
-            
+            BodyPart attatchmentBodyPart = new MimeBodyPart();            
 
             // set contnets for text message and attatchment bodyparts
             messageBodyPart.setText(textMessage); 
@@ -65,13 +70,22 @@ public class EmailSender {
 
             // Helps Sends Message through smtp protocol
             Transport transport =  mailSession.getTransport("smtp");
-            transport.connect("smtp.office365.com", username, password);
+            transport.connect("smtp.office365.com", username, password);            
 
             transport.sendMessage(emailMessage, emailMessage.getAllRecipients());
 
             transport.send(emailMessage);
-        } catch (MessagingException ex) {
-            Logger.getLogger(EmailSender.class.getName()).log(Level.SEVERE, null, ex);
+            
+            output.setText("EMAIL SENT");            
+            
+        } catch (AddressException e) {
+                e.printStackTrace();
+                
+                output.setText("Username or Password Mispelled or Missing");
+        } catch (MessagingException e) {
+            
+                output.setText("Email not sent");
+                e.printStackTrace();
         }
     }
 }
